599ee6f080d9e288d8c8f818ca1cd6ebf84987ea
Jayashankar <jayashankarkanigiri@gmail.com>
2023-12-20 20:03:13 +0530

    extract data fetching logic into a custom hook useShowById

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react'; // Import useState from React
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 
-const Show = () => {
-  const { showId } = useParams();
-
+const useShowById = (showId) => {
   const [showData, setShowData] = useState(null);
   const [showError, setShowError] = useState(null);
 
@@ -20,6 +18,16 @@ const Show = () => {
 
     fetchData();
   }, [showId]);
+
+  return { showData, showError }
+
+
+}
+
+
+const Show = () => {
+  const { showId } = useParams();
+  const { showData, showError } = useShowById(showId)
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
