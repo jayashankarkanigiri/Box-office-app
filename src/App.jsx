a553b396c22b05d38f78dbfbe7b98f0fdcec76e9
Jayashankar <jayashankarkanigiri@gmail.com>
2023-12-27 09:19:06 +0530

    deployed the app to github pages. use HashRouter. use Link instead of native <a>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
@@ -17,7 +17,7 @@ function App() {
       <GlobalTheme>
 
 
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
@@ -28,7 +28,7 @@ function App() {
         <Route path="*" element={<div>Not found</div>} />
         
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </GlobalTheme>
 
     </QueryClientProvider>
